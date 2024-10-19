08127f3a76e3ceb666cea385b99c3684973df257
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2024-10-20 00:51:34 +0300

    Update data source

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -11,7 +11,7 @@ export class ChartsComponent {
   this.cards = [
       { title: 'Вантажна партія 1', cols: 1, rows: 1 },
       { title: 'Вантажна партія 2', cols: 1, rows: 1 },
-      { title: 'Обробка вантажів по порту', cols: 2, rows: 1 },
+      { title: 'Кількість авто з вантажем на експорт за місяць', cols: 2, rows: 2 },
     ];
   }
 }
